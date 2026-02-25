1010aa0be0716a5f40709c737fb71a27b1d2cc78
AjayKumarN-2255 <kumar.ajay@midnay.com>
2026-02-25 10:18:07 +0530

    completed home page

---
+++ astro.config.mjs
@@ -0,0 +1,5 @@
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  // no need for Vite plugin for Tailwind v3
+});
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        handwriting: ['Kalam', 'cursive'],
+      },
+      colors: {
+        primary: "#00715D",
+        secondary: "#FBD45B",
+        heading: "#2A3936",
+        paragraph: "#555555",
+        highlight: "#ECECEC"
+      },
+    },
+  },
+  plugins: [],
+};
+
