cfa6e768d8041598c08557e922e129dccdb11d1c
AjayKumarN-2255 <kumar.ajay@midnay.com>
2026-02-26 18:05:18 +0530

    overall/about section completed

---
+++ src/data/about/need_data.js
@@ -0,0 +1,21 @@
+
+export const homeSections = [
+    {
+        title: "Education Support",
+        content: `Providing access to quality education, learning tools, and mentorship for children.`,
+        image: "/src/assets/home/image_3.jpg",
+        linkText: "Explore Now"
+    },
+    {
+        title: "Health & Nutrition",
+        content: `Ensuring children receive medical care, proper nutrition, and wellness support.`,
+        image: "/src/assets/home/image_2.jpg",
+        linkText: "Explore Now"
+    },
+    {
+        title: "Child Protection",
+        content: `Creating safe environments and protecting children from harm and neglect.`,
+        image: "/src/assets/home/image_1.jpg",
+        linkText: "Explore Now"
+    },
+];
