53635527e697f80ff7f152d54c90a21fa96ecbde
AjayKumarN-2255 <kumar.ajay@midnay.com>
2026-02-26 09:45:32 +0530

    completed driven/home section

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,11 @@ export default {
         heading: "#2A3936",
         paragraph: "#555555",
         highlight: "#ECECEC",
-        card:"#F9F4E8"
+        card: "#F9F4E8"
+      },
+      spacing: {
+        div_lg_gap: '120px',
+        div_sm_gap: '80px',
       },
     },
   },
