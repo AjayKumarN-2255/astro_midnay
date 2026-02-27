ff5f124a61101815ae2561927e954b5aba1b685e
AjayKumarN-2255 <kumar.ajay@midnay.com>
2026-02-27 14:51:04 +0530

    completed opportunities/involved section

---
+++ src/data/involved/opp_data.js
@@ -0,0 +1,17 @@
+export const opportunities = [
+    {
+        title: "Youth Mentor",
+        content: "Guide and support students through our after-school programs.",
+        type: "2-4hr/week"
+    },
+    {
+        title: "Event Staff",
+        content: "Help coordinate logistics and guest services at our fundraisers.",
+        type: "Weekends"
+    },
+    {
+        title: "Pro-bono Legal",
+        content: "Provide legal advice to families in need of housing support.",
+        type: "2-4hr/week"
+    }
+];
