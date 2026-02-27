6ed7273224090fa95544f5ee4aacfe69edd53718
AjayKumarN-2255 <kumar.ajay@midnay.com>
2026-02-27 12:49:02 +0530

    completed fund/involved section

---
+++ src/data/involved/fund_data.js
@@ -0,0 +1,17 @@
+export const homeSections = [
+    {
+        title: "Annual Fall Gala",
+        content: `There are many of lorem, but majori have suffered alteration in some form.`,
+        image: "/src/assets/home/image_3.jpg"
+    },
+    {
+        title: "Charity 5K Run",
+        content: `There are many of lorem, but majori have suffered alteration in some form.`,
+        image: "/src/assets/home/image_2.jpg"
+    },
+    {
+        title: "Art Auction",
+        content: `There are many of lorem, but majori have suffered alteration in some form.`,
+        image: "/src/assets/home/image_1.jpg"
+    },
+];
