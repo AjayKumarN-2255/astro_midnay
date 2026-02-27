a848019d709efd277def35bd342b40b6672c5d6f
AjayKumarN-2255 <kumar.ajay@midnay.com>
2026-02-27 11:37:00 +0530

    completed banner,driven,services -- wedo page

---
+++ src/data/wedo/service_data.js
@@ -0,0 +1,20 @@
+export const homeSections = [
+    {
+        title: "Education for a Better Tomorrow",
+        content: `We support children's education by providing school materials, learning spaces, mentorship, and access to quality education programs.`,
+        image: "/src/assets/home/image_3.jpg",
+        linkText: "Explore Now"
+    },
+    {
+        title: "Health, Care & Nutrition",
+        content: `Our health initiatives focus on medical support, nutrition programs, and wellness care to help children grow strong and healthy.`,
+        image: "/src/assets/home/image_2.jpg",
+        linkText: "Explore Now"
+    },
+    {
+        title: "Child Protection & Welfare",
+        content: `We work to protect children from neglect and unsafe conditions by creating secure environments and support systems.`,
+        image: "/src/assets/home/image_1.jpg",
+        linkText: "Explore Now"
+    },
+];
