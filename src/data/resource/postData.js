7a1a22a789e5774becc048daa13f606f849d4b8f
AjayKumarN-2255 <kumar.ajay@midnay.com>
2026-03-02 14:56:47 +0530

    completed Lists/resource section

---
+++ src/data/resource/postData.js
@@ -0,0 +1,49 @@
+export const postData = [
+    {
+        title: "It is a long established fact that a reader",
+        content: `There are many of lorem, but majori have suffered alteration in some form.`,
+        image: "/src/assets/home/image_3.jpg"
+    },
+    {
+        title: "It is a long established fact that a reader",
+        content: `There are many of lorem, but majori have suffered alteration in some form.`,
+        image: "/src/assets/home/image_2.jpg"
+    },
+    {
+        title: "It is a long established fact that a reader",
+        content: `There are many of lorem, but majori have suffered alteration in some form.`,
+        image: "/src/assets/home/image_1.jpg"
+    },
+
+    {
+        title: "It is a long established fact that a reader",
+        content: `There are many of lorem, but majori have suffered alteration in some form.`,
+        image: "/src/assets/home/image_3.jpg"
+    },
+    {
+        title: "It is a long established fact that a reader",
+        content: `There are many of lorem, but majori have suffered alteration in some form.`,
+        image: "/src/assets/home/image_1.jpg"
+    },
+    {
+        title: "It is a long established fact that a reader",
+        content: `There are many of lorem, but majori have suffered alteration in some form.`,
+        image: "/src/assets/home/image_2.jpg"
+    },
+
+    {
+        title: "It is a long established fact that a reader",
+        content: `There are many of lorem, but majori have suffered alteration in some form.`,
+        image: "/src/assets/home/image_3.jpg"
+    },
+    {
+        title: "It is a long established fact that a reader",
+        content: `There are many of lorem, but majori have suffered alteration in some form.`,
+        image: "/src/assets/home/image_2.jpg"
+    },
+    {
+        title: "It is a long established fact that a reader",
+        content: `There are many of lorem, but majori have suffered alteration in some form.`,
+        image: "/src/assets/home/image_1.jpg"
+    }   
+];
